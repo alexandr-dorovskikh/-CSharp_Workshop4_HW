4509c56af0a523d732bbd7d2f48676960cc2a1fa
Alexandr Dorovskikh <alexandr.dorovskikh@gmail.com>
2022-11-27 13:34:05 +0700

    Task 27 added

---
+++ Task 27/Program.cs
@@ -0,0 +1,20 @@
+﻿int NumDigitsSum(int num)
+{
+    int Result = 0;
+
+    while (num > 0)
+    {
+        int LastDigit = num % 10;        
+        
+        Result += LastDigit;
+        num = num / 10;     
+    
+    }
+
+    return Result;
+}
+
+Console.Write("num = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"digits sum = {NumDigitsSum(num)}");
